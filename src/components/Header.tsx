f5a869c58fc44660682b10e36036e32f0822ad20
Sabbo Sachi Banik <sabbosachibanik@gmail.com>
2026-09-13 12:37:02 +0600

    Header Design Updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,23 +3,27 @@ import logo from "../assets/logo-text.png";
 
 const Header = () => {
     return (
-        <header>
-            <nav>
+        <header className='p-3'>
+            <nav className= 'container mx-auto flex justify-between items-center'>
                 <div>
                     <img src={logo} alt="Logo" />
                 </div>
                 <div>
-                    <ul>
-                        <li>Home</li>
-                        <li>Technologies</li>
-                        <li>Projects</li>
-                        <li>About</li>
-                        <li>Contact</li>
+                    <ul className='flex gap-5 items-center'>
+                        <a href=""><li>Home</li></a>
+                        <a href=""><li>Technologies</li></a>
+                        <a href=""><li>Projects</li></a>
+                        <a href=""><li>About</li></a>
+                        <a href=""><li>Contact</li></a>
                     </ul>
                 </div>
-                <div>
-                    <button className="btn btn-soft btn-secondary">Sign In</button>
-                    <button className="btn btn-soft btn-secondary">Sign Up</button>
+                <div className='flex gap-3'>
+                    <button className="px-4 py-1 border border-transparent rounded-4xl
+                       bg-transparent text-black
+                       hover:bg-[#D91B7E] hover:text-white">Sign In</button>
+                    <button className="px-4 py-1 border-2 border-[#D91B7E] rounded-4xl
+                       bg-[#D91B7E] text-white
+                       hover:bg-transparent hover:text-[#D91B7E]">Sign Up</button>
                     
                 </div>
             </nav>
