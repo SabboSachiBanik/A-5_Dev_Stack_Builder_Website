5dd8d179a8d5f9c231c4af8ce3bf5a574a5f48d8
Sabbo Sachi Banik <sabbosachibanik@gmail.com>
2026-09-13 14:05:12 +0600

    Banner Design Updated

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,20 +3,18 @@ import BannerImg from "../assets/banner-stack.png";
 
 const Banner = () => {
     return (
-        <section className='container mx-auto py-15 grid grid-cols-2'>
+        <section className='container mx-auto pt-23 pb-5 grid grid-cols-2'>
             <div className=' '>
-            <h1>Build Your Ideal <span>Development</span><span> Stack</span></h1>
-            <p>Explore frontend, backend, database, and tooling options,
+            <h1 className='text-5xl font-bold mt-20'>Build Your Ideal <br /><span className='bg-gradient-to-r from-[#FF5722] via-[#D81B7E] to-[#7C3AED] bg-clip-text text-transparent'>Development Stack</span></h1>
+
+            <p className='text-[#475569] py-10'>Explore frontend, backend, database, and tooling options,
             compare them side by side, and put together the stack that fits your
             next project.</p>
 
-            <div>
-                 <button className="px-4 py-1 border-2 border-[#D91B7E] rounded-4xl
-                       bg-[#D91B7E] text-white
-                       hover:bg-transparent hover:text-[#D91B7E]">Explore Technologies</button>
-            <button className="px-4 py-1 border border-transparent rounded-4xl
-                       bg-transparent text-black
-                       hover:bg-[#D91B7E] hover:text-white">Learn MoreSign In</button>
+            <div className='grid grid-cols-3 flex gap-3 items-center'>
+                 <button className=" w-full py-2 border  rounded-md bg-gradient-to-r from-[#F97316] to-[#EC4899] text-white hover:bg-none hover:bg-transparent hover:border-[#cad1dc] hover:text-black transition-colors]">Explore Technologies</button>
+            <button className="w-full py-2 border border-1 border-[#cad1dc] rounded-md text-black bg-transparent
+                       hover:bg-gradient-to-r from-[#F97316] to-[#EC4899] hover: bg-[#D91B7E] hover:text-white">Learn More</button>
 
             </div>
 
@@ -24,8 +22,8 @@ const Banner = () => {
 
             {/* Right Side --> */}
            
-            <div>
-                <img src={BannerImg} alt="Banner Image" />
+            <div className='flex items-center '>
+                <img className='w-full h-auto max-h-[450px] object-cover' src={BannerImg} alt="Banner Image" />
             </div>
         </section>
     );
